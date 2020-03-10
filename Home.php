b2e089947ce8112869201fed747272308ef19635
vishwakirtivardhan <vishwakirtimaster@gmail.com>
2020-03-10 17:11:06 +0530

    add Home files

---
+++ Home.php
@@ -0,0 +1,31 @@
+<html>
+<head>
+<title>Welcome to the  </title>
+ <script src="js/jquery.min.js"></script>
+        <script src="js/bootstrap.bundle.js"></script>
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <script src="js/bootstrap.js"></script>
+        <script src="js/bootstrap.min.js"></script>
+        <!-- This is the bootstrap links--->
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/bootstrap-grid.css">
+        <link rel="stylesheet" href="css/bootstrap-reboot.css">
+        <link rel="stylesheet" href="css/bootstrap-reboot.min.css">
+        <link rel="stylesheet" href="css/bootstrap-grid.min.css">
+
+</head>
+<body>
+<div class="container" style="position: fixed;
+  top: 50%;
+  left: 50%;
+  margin-top: -50px;
+  margin-left: -100px;
+transform: translate(-50%, -50%)
+  ">
+	<img src="image/main.jpg" style="max-height: 300px; width: 100%;height: auto;" class="img-fluid">
+	<input type="link" name="url" class="w-100"  />
+</div>
+
+</body>
+</html>
